18451425dd43c621ea77cc3d33e3259ae502f25b
Alex McClung <alex.mcclung@hotmail.com>
2016-06-19 00:20:05 +1000

    Converted to PEP8 compliant

--- src/rsync_node.py
+++ src/rsync_node.py
@@ -6,35 +6,39 @@ from subprocess import Popen, PIPE
 import actionlib
 from rsync_ros.msg import RsyncAction, RsyncResult
 
+
 class RsyncServer:
-  def __init__(self):
-    self.server = actionlib.SimpleActionServer('rsync', RsyncAction, self.execute, False)
-    self.server.start()
 
-  def execute(self, goal):
-  	command = ['rsync'] + goal.rsync_args + [goal.source_path, goal.destination_path]
+    def __init__(self):
+        self.server = actionlib.SimpleActionServer(
+            'rsync', RsyncAction, self.execute, False)
+        self.server.start()
 
-	rospy.loginfo("Executing rsync command '%s'", ' '.join(command))
+    def execute(self, goal):
+        command = ['rsync'] + goal.rsync_args + \
+            [goal.source_path, goal.destination_path]
 
-	p = Popen(command,shell=False,stdout=PIPE,stderr=PIPE)
-	stdout, stderr = p.communicate()
-	rospy.loginfo(stdout)
-	
-	if stderr:
-		rospy.logerr(stderr)
-	
-	result = RsyncResult()
-	result.sync_success = p.returncode > -1
+        rospy.loginfo("Executing rsync command '%s'", ' '.join(command))
 
-	rospy.loginfo("Rsync command result '%s'", result.sync_success)
+        p = Popen(command, shell=False, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = p.communicate()
+        rospy.loginfo(stdout)
 
-	self.server.set_succeeded(result)
+        if stderr:
+            rospy.logerr(stderr)
+
+        result = RsyncResult()
+        result.sync_success = p.returncode > -1
+
+        rospy.loginfo("Rsync command result '%s'", result.sync_success)
+
+        self.server.set_succeeded(result)
 
 if __name__ == "__main__":
-	try:
-		rospy.init_node('rsync_actionlib')
-		rospy.loginfo("Ready to sync files.")
-		RsyncServer()
-		rospy.spin()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        rospy.init_node('rsync_actionlib')
+        rospy.loginfo("Ready to sync files.")
+        RsyncServer()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
